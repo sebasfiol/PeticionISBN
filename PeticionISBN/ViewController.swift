e6dc4049cbf75273ae6a736f63f49cd1243280b1
Sebastià Fiol <sfiol@efeefe.com>
2016-08-01 13:09:42 +0200

    Añadir tratamiento JSON

--- PeticionISBN/ViewController.swift
+++ PeticionISBN/ViewController.swift
@@ -28,6 +28,21 @@ class ViewController: UIViewController {
             self.presentViewController(alertController, animated: true, completion: nil)
             
         } else {
+/*            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                let dico2 = dico1["ISBN"] as! NSDictionary
+                let dico3 = dico2["title"] as! NSString as String
+                let dico4 = dico3["authors"] as! NSDictionary
+                self.ciudad.text = dico5["name"] as! NSString as String
+                let dico6 = dico4["item"] as! NSDictionary
+                let dico7 = dico6["condition"] as! NSDictionary
+//                self.temperatura.text = dico7["temp"] as! NSString as String
+            }
+            catch _ {
+                
+            }
+*/
         let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)!
         detalleISBN.text = String(texto)
         }
@@ -46,4 +61,6 @@ class ViewController: UIViewController {
 
 
 }
+
+
 
