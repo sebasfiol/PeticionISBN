5c55dba76ba86c2ee52708248965990606523756
Sebastià Fiol <sfiol@efeefe.com>
2016-08-02 09:55:49 +0200

    Añadidos los campos de Título, autores y portada libro. Se han añadido los elementos gráficos necesarios para que se vieran estos nuevos campos.

--- PeticionISBN/ViewController.swift
+++ PeticionISBN/ViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var numeroISBN: UITextField!
-    @IBOutlet weak var detalleISBN: UITextView!
+    @IBOutlet weak var tituloLibro: UILabel!
+    @IBOutlet weak var autores: UITextView!
+    @IBOutlet weak var portadaLibro: UIImageView!
     
     @IBAction func buscarLibros(sender: AnyObject) {
-        detalleISBN.text = ""
+        self.tituloLibro.text = ""
+        self.autores.text = ""
         var urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
         let isbn = numeroISBN.text!
         urls = "\(urls)\(isbn)"
         let url = NSURL(string: urls)
-        let datos:NSData? = NSData(contentsOfURL: url!)
+        let datos = NSData(contentsOfURL: url!)
         if datos == nil {
             let alertController = UIAlertController(title: "Error", message:"Se ha producido un error de comunicación.", preferredStyle: UIAlertControllerStyle.Alert)
             
@@ -28,26 +31,43 @@ class ViewController: UIViewController {
             self.presentViewController(alertController, animated: true, completion: nil)
             
         } else {
-/*            do {
+          do {
                 let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
                 let dico1 = json as! NSDictionary
-                let dico2 = dico1["ISBN"] as! NSDictionary
-                let dico3 = dico2["title"] as! NSString as String
-                let dico4 = dico3["authors"] as! NSDictionary
-                self.ciudad.text = dico5["name"] as! NSString as String
-                let dico6 = dico4["item"] as! NSDictionary
-                let dico7 = dico6["condition"] as! NSDictionary
-//                self.temperatura.text = dico7["temp"] as! NSString as String
+                let dico2 = dico1["ISBN:"+isbn] as! NSDictionary
+                self.tituloLibro.text = dico2["title"] as! NSString as String
+                //let dico3 = dico2["authors"] as! NSArray
+                if let dico3 = dico2["authors"] as? [[String: AnyObject]] {
+                    for autor in dico3 {
+                        if let name = autor["name"] as? String {
+                            self.autores.insertText(name)
+                        }
+                    }
+                }
+                if let dico4 = dico2["cover"] as? NSDictionary {
+                    let dico5 = dico4["medium"] as! NSString as String
+                        cargarImagen(dico5)
+                }
             }
             catch _ {
                 
             }
-*/
-        let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)!
-        detalleISBN.text = String(texto)
+        //let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)!
+        //detalleISBN.text = String(texto)
         }
         self.view.endEditing(true)
     }
+    
+    func cargarImagen(urlString:String)
+    {
+        let url = NSURL(string: urlString)
+        let datos = NSData(contentsOfURL: url!)
+        if datos == nil {
+            self.portadaLibro.image = UIImage(data: datos!)
+        }
+        
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
